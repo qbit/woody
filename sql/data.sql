051ecf4c4ff60b7b11551ab482dce31cc2b5402c
Aaron Bieber <deftly@gmail.com>
2013-07-09 10:24:19 -0600

    consolidated sql

---
+++ sql/data.sql
@@ -0,0 +1,7 @@
+insert into hold_types ( name ) values ( 'Jug' );
+insert into hold_types ( name ) values ( 'Mini-Jug' );
+insert into hold_types ( name ) values ( 'Sloper' );
+insert into hold_types ( name ) values ( 'Pocket' );
+insert into hold_types ( name ) values ( 'Crimp' );
+insert into hold_types ( name ) values ( 'Volume' );
+
